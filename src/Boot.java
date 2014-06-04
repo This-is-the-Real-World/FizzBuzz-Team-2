f2759cb2ae910d49f393128760f2b01e8c3b41ec
matt12390123 <divislyfe@gmail.com>
2014-06-04 17:11:20 +0100

    Edited Boot.java. Pretty pictures.

--- src/Boot.java
+++ src/Boot.java
@@ -1,15 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Jake on 04/06/2014.
  */
-public class Boot {
+public class Boot extends JFrame {
+
+    private Image loadingImage;
+
+    public Boot() {
+        try {
+            loadingImage = Toolkit.getDefaultToolkit().createImage(new URL("http://i.imgur.com/LcWmfbD.gif"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        getContentPane().setBackground(Color.BLACK);
+        getContentPane().add(new JLabel(new ImageIcon(loadingImage)));
+    }
 
     public static void main(String[] args) {
-        System.out.println(Math.random());
-
-        try {
-            throw new Exception();
-        } catch(Exception a) {
-            System.out.println("Exceptions");
-        }
+        final Boot boot = new Boot();
+        boot.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        boot.pack();
+        boot.setVisible(true);
     }
 }
