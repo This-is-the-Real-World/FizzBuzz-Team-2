0e016ebb895c044affc87d542d3c2d85749d88cf
sosweaty <sosweaty@hotmail.com>
2014-06-11 15:17:02 +0100

    Minor Changes

--- src/io/github/sosweaty/fizzbuzz/FizzBuzzComplex.java
+++ src/io/github/sosweaty/fizzbuzz/FizzBuzzComplex.java
@@ -2,22 +2,37 @@ package io.github.sosweaty.fizzbuzz;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class FizzBuzzComplex extends JPanel implements ActionListener {
+class FizzBuzzComplex extends JPanel {
 
     private final static String newline = "\n";
-    protected JTextArea textArea;
-    protected JButton myButton;
+    private JTextArea textArea;
 
-    public FizzBuzzComplex() {
+    private FizzBuzzComplex() {
         super(new GridBagLayout());
 
-        myButton = new JButton("Start FizzBuzz");
-        myButton.addActionListener(this);
+        JButton myButton = new JButton("Start FizzBuzz");
+        myButton.addActionListener(e -> {
+            //targets set i to 0 if i is less than 1000 than add 1 to i
+            for (int i = 0; i <= 1000; i++)
+                //if i is a multiple of 3 & 5 print fizzbuzz + i
+                if (i % 3 == 0 && i % 5 == 0)
+                    textArea.append("FizzBuzz :" + i + newline);
 
-        textArea = new JTextArea();
+                    //else if i is a multiple of 3 print fizz + i
+                else if (i % 3 == 0)
+                    textArea.append("Fizz :" + i + newline);
+
+                    //else if i is a multiple of 5 print buzz + i
+                else if (i % 5 == 0)
+                    textArea.append("Buzz :" + i + newline);
+                    //else if i is not a multiple of 5 and 3 print i
+                else
+                    textArea.append(i + newline);
+
+        });
+
+        textArea = new JTextArea(10, 30);
         textArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(textArea);
 
@@ -25,7 +40,7 @@ public class FizzBuzzComplex extends JPanel implements ActionListener {
         GridBagConstraints c = new GridBagConstraints();
         c.gridwidth = GridBagConstraints.REMAINDER;
 
-        c.fill = GridBagConstraints.BOTH;
+        c.fill = GridBagConstraints.HORIZONTAL;
         add(myButton, c);
 
         c.fill = GridBagConstraints.BOTH;
@@ -43,35 +58,12 @@ public class FizzBuzzComplex extends JPanel implements ActionListener {
         frame.add(new FizzBuzzComplex());
 
         //Sets Frame to 300px, 300px & visible
-        frame.setSize(300, 300);
+        frame.pack();
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         //Create and show the GUI
         javax.swing.SwingUtilities.invokeLater(FizzBuzzComplex::createAndShowGUI);
-    }
-
-    public void actionPerformed(ActionEvent evt) {
-        //If Button pressed
-        if (evt.getSource() == myButton) {
-
-            //targets set i to 0 if i is less than 1000 than add 1 to i
-            for (int i = 0; i <= 1000; i++)
-                //if i is a multiple of 3 & 5 print fizzbuzz + i
-                if (i % 3 == 0 && i % 5 == 0)
-                    textArea.append("FizzBuzz: " + i + newline);
-
-                    //else if i is a multiple of 3 print fizz + i
-                else if (i % 3 == 0)
-                    textArea.append("Fizz: " + i + newline);
-
-                    //else if i is a multiple of 5 print buzz + i
-                else if (i % 5 == 0)
-                    textArea.append("Buzz: " + i + newline);
-                    //else if i is not a multiple of 5 and 3 print i
-                else
-                    textArea.append(i + newline);
-        }
     }
 }
